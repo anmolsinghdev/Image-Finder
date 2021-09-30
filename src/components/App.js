46ecbfca57b6377de3eaf4c17c87502300a8b4fe
ohboy <singhchauhananmol500@gmail.com>
2021-09-30 17:45:46 +0530

    Adding Loader

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,30 @@ import React from 'react';
 import unsplash from '../api/unsplash';
 import ImageList from './ImageList';
 import SearchBar from './SearchBar';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  state = { images: [] };
+  state = { images: [], loading: false };
 
   onSearchSubmit = async (term) => {
+    this.setState({ loading: true });
+
     const response = await unsplash.get('/search/photos', {
       params: { query: term },
     });
 
-    this.setState({ images: response.data.results });
+    this.setState({ images: response.data.results, loading: false });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '1rem' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images} />
+        {this.state.loading ? (
+          <Spinner />
+        ) : (
+          <ImageList images={this.state.images} />
+        )}
       </div>
     );
   }
